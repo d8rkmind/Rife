83f233e2129add4dd2a45ad021d3de890c248941
d8rkmind <syamsv01@gmail.com>
2022-08-24 12:29:54 +0530

    Devnull error

--- src/utils/installscript.py
+++ src/utils/installscript.py
@@ -1,6 +1,6 @@
 from subprocess import call
 from os import devnull
-Devnull = devnull()
+Devnull = open (devnull, 'w')
 from src.constants import PKG_DOWNLOAD_PATH
 def install():
     call(["dpkg","-i",f"{PKG_DOWNLOAD_PATH}*"], stdout=Devnull, stderr=Devnull)
